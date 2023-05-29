920401d3369973a349ffd9f450c4d790545b3ea9
vk332c <70476666+vk332c@users.noreply.github.com>
2023-05-29 19:21:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ async def on_ready():
         for member in guild.members:
             userlist.append(member)
     
-    cursor.execute("CREATE TABLE IF NOT EXISTS users_online_stats(id INT,username TEXT, lastActive INT , online INT, idle INT, dnd INT, offline INT)")
-    cursor.execute("CREATE TABLE IF NOT EXISTS users_voice_stats(id INT, username TEXT, serverid INT, lastActive INT, voicetime INT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS users_online_stats(id INT, username TEXT, lastActive INT, online INT, idle INT, dnd INT, offline INT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS users_voice_stats(id INT, username TEXT, server_id INT, lastJoined INT, voice_time INT)")
     for user in userlist:
         check = cursor.execute("SELECT * FROM users_online_stats WHERE id = ?", (user.id,))
         if check.fetchone() is None:
@@ -29,6 +29,41 @@ async def on_ready():
     conn.commit()
     print(f'\n\nLogged in as {bot.user} (ID: {bot.user.id})')
     print('------\n\n')
+
+
+def getvoicevalue(value):
+    minutes = value / 60
+    hours = minutes / 60
+    if value < 60:
+        value = str(round(value)) + 's'
+    elif round(minutes) < 60:
+        value = str(round(minutes)) + 'm'
+    else:
+        value = str(round(hours, 1)) + 'h'
+    return value
+
+def getvalue(values,arg):
+    timings = []
+    total = 0
+    for v in values:
+        total = total + v
+    for value in values:
+        minutes = value / 60
+        hours = minutes / 60
+        percent = value / total * 100
+        if value < 60:
+            value = str(round(value)) + f's ({round(percent,1)}%)'
+            timings.append(value)
+        elif round(minutes) < 60:
+            value = str(round(minutes)) + f'm ({round(percent,1)}%)'
+            timings.append(value)
+        else:
+            value = str(round(hours, 1)) + f'h ({round(percent,1)}%)'
+            timings.append(value)
+    return timings[arg]
+
+
+
 
 @bot.event
 async def on_presence_update(before,after):
@@ -41,6 +76,27 @@ async def on_presence_update(before,after):
             if str(before.status) == status:
                 cursor.execute(f'UPDATE users_online_stats SET lastActive = ?, {status} = {status} + ? WHERE id = ?', (round(time.time()),seconds, before.id))
                 conn.commit()
+
+
+@bot.event
+async def on_voice_state_update(member,before,after):
+    cursor.execute('SELECT id,server_id FROM users_voice_stats WHERE id = ? AND server_id = ?', (member.id, member.guild.id))
+    if cursor.fetchone() is None:
+        cursor.execute('INSERT INTO users_voice_stats VALUES(?,?,?,?,?)', (member.id, member.name, after.channel.guild.id,round(time.time()), 0))
+        conn.commit()
+    else:
+        cursor.execute('SELECT lastJoined FROM users_voice_stats WHERE id = ? AND server_id = ?', (member.id, member.guild.id))
+        lastJoined = cursor.fetchone()[0]
+        seconds = round(time.time()) - lastJoined
+        if before.channel is not None and after.channel is None:
+            cursor.execute('UPDATE users_voice_stats SET lastJoined = ?, voice_time = voice_time + ? WHERE id = ? AND server_id = ?', (0, seconds, member.id, before.channel.guild.id))
+            conn.commit()
+        elif before.channel is None and after.channel is not None:
+            cursor.execute('UPDATE users_voice_stats SET lastJoined = ? WHERE id = ? AND server_id = ?', (round(time.time()), member.id, after.channel.guild.id))
+            conn.commit()
+
+
+
 
 @bot.command()
 async def activity(ctx, member: nextcord.Member = None):
@@ -56,26 +112,6 @@ async def activity(ctx, member: nextcord.Member = None):
             conn.commit()
     cursor.execute('SELECT online,idle,dnd,offline FROM users_online_stats WHERE id = ?', (member.id,))
     values = cursor.fetchone()
-    def getvalue(values, arg):
-        timings = []
-        total = 0
-        for v in values:
-            total = total + v
-        for value in values:
-            minutes = value / 60
-            hours = minutes / 60
-            percent = value / total * 100
-            if value < 60:
-                value = str(round(value)) + f's ({round(percent,1)}%)'
-                timings.append(value)
-            elif round(minutes) < 60:
-                value = str(round(minutes)) + f'm ({round(percent,1)}%)'
-                timings.append(value)
-            else:
-                value = str(round(hours, 1)) + f'h ({round(percent,1)}%)'
-                timings.append(value)
-        return timings[arg]
-        print(total)
 
     values_list = list(values)
     labels = ['Online', 'Idle', 'Do not disturb', 'Offline']
@@ -97,7 +133,41 @@ async def activity(ctx, member: nextcord.Member = None):
     plt.savefig(f'{member.id}.png')
     embed = nextcord.Embed(title = f'{member.display_name}', color = member.color)
     embed.add_field(name = 'Stats',value = f'🟢 Online: **{getvalue(values,0)}**\n🌙 Idle: **{getvalue(values,1)}**\n⛔ Do not disturb: **{getvalue(values,2)}**\n😴 Offline: **{getvalue(values,3)}**')
-    embed.set_footer(text=f"Data taken from {utctime} UTC")
+    embed.set_footer(text=f"Data taken from {utctime}")
     await ctx.send(embed=embed,file = nextcord.File(f'{member.id}.png'))
     os.remove(f'{member.id}.png')
+
+
+@bot.command()
+async def voicestats(ctx, member: nextcord.Member = None):
+    if member is None:
+        member = ctx.author
+    cursor.execute('SELECT id, server_id FROM users_voice_stats WHERE id = ? and server_id = ?', (member.id, member.guild.id))
+    if cursor.fetchone() is None:
+        embed = nextcord.Embed(title = f'Voice activity statistic', color = member.color)
+        embed.set_thumbnail(url = member.guild.icon)
+        embed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
+        embed.add_field(name = '',value =f"{member.display_name} not сonnected to any voice channel\nTotal time: **0s**")
+        embed.set_footer(text=f"Data taken from {utctime}")
+        await ctx.send(embed=embed)
+    else:
+        cursor.execute('SELECT lastJoined,voice_time FROM users_voice_stats WHERE id = ? and server_id = ?', (member.id, member.guild.id))
+        data = cursor.fetchone()
+        lastJoined = data[0]
+        voice_time = data[1]
+        if member.voice is None:
+            embed = nextcord.Embed(title = f'Voice activity statistic', color = member.color)
+            embed.set_thumbnail(url = member.guild.icon)
+            embed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
+            embed.add_field(name = '',value =f"{member.display_name} not сonnected to any voice channel\nTotal time: **{getvoicevalue(voice_time)}**")
+            embed.set_footer(text=f"Data taken from {utctime}")
+            await ctx.send(embed=embed)
+        else:
+            embed = nextcord.Embed(title = f'Voice activity statistic', color = member.color)
+            embed.set_thumbnail(url = member.guild.icon)
+            embed.set_author(name = member.display_name, icon_url = member.display_avatar.url)
+            embed.add_field(name = '',value =f"{member.display_name} connected to the voice channel ***{member.voice.channel.name}***\nСurrent time in the voice channel: **{getvoicevalue(time.time() - lastJoined)}**\nTotal time: **{getvoicevalue(time.time() - lastJoined + voice_time)}**")
+            embed.set_footer(text=f"Data taken from {utctime}")
+            await ctx.send(embed=embed)
+
 bot.run(token)
